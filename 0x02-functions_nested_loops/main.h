82c587493b11da16b1232af52e2a4549bdc5060a
root <berishabaye@gmail.com>
2023-02-21 22:25:31 -0800

    0_putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+void _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void _islower(int c);
+void _isalpha(int  c);
+void print_sign(int n);
+void _abs(int);
+void print_last_digit(int);
+void _24_hours(void);
+void times_table(void);
+void _add(int,int);
+void print_to_98(int n);
+
