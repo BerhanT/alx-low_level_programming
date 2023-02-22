82f3948037d075bd4787331dae601986a5752498
root <berishabaye@gmail.com>
2023-02-22 04:06:34 -0800

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - prints alphabet in lower case 10x
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+	i=0;
+	while(i<10)
+	{
+		c='a';
+		while(c<='z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
