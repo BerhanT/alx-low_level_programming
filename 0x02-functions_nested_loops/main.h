1ba06289a67686c441049e86e469d278a03baf9a
BerhanT <berishabaye@gmail.com>
2023-03-01 02:50:57 -0800

    8-24_hours

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ int _isalpha(int c);
 int  print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void _24_hours(void);
+void jack_bauer(void);
 void times_table(void);
 void _add(int,int);
 void print_to_98(int n);
