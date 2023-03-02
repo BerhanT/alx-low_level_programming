65c8d6a30d02dcc5ddb5fd9e7ddd33f4d5cc123c
BerhanT <berishabaye@gmail.com>
2023-03-02 01:14:16 -0800

    102-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	long b = 1;
+	long c = 2;
+	long sum = c;
+
+	while (b + c < 4000000)
+	{
+		c += b;
+		if (c % 2 == 0)
+			sum += c;
+		b = c - b;
+		++i;
+	}
+	printf("%ld", sum);
+	return (0);
+}
