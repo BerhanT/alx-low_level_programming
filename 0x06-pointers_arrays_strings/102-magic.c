a082a9f2c4209bdb5ac1f967f7beb435ec687453
BerhanT <berishabaye@gmail.com>
2023-03-04 01:08:46 -0800

    more pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - majic code
- * Return: always 0
- */
-
 int main(void)
 {
 	int n;
@@ -14,12 +9,15 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 /*
- * You are not allowed to use the variable a in your new line of code
- * You are not allowed to modify the variable p
- * You can only write one statement
- * You are not allowed to use ,
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
  */
 	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
